4f29627ea17a56a42c93d7354d2f8a595e808851
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-02-17 15:16:49 -0600

    implement speed-up

---
+++ hw1/p3/part_1/tst_h2d.m
@@ -0,0 +1,14 @@
+
+
+e2=1;
+t0=tic;
+N=80;  dt=.16000; heat2d
+N=80;  dt=.08000; heat2d
+N=80;  dt=.04000; heat2d
+N=80;  dt=.02000; heat2d
+N=80;  dt=.01000; heat2d
+N=80;  dt=.00500; heat2d
+N=80;  dt=.00250; heat2d
+N=80;  dt=.00125; heat2d
+etime=toc(t0)
+
